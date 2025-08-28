8511ac595fe0e24f33f8c6c08d97bdc002874d64
Thomas Turner <thomas_turner@talktalk.net>
2025-08-28 16:30:17 +0100

    Revert "ANDROID: mnt: Add filesystem private data to mount points"
    This reverts commit a618d31ac826e1624836e6fb7d5a2647b23ae6bb.
    
    Change-Id: Iab581f7675bcb4b8e4299f5e5fa045f51699f821

--- fs/pnode.c
+++ fs/pnode.c
@@ -678,29 +678,3 @@ int propagate_umount(struct list_head *list)
 
 	return 0;
 }
-
-void propagate_remount(struct mount *mnt)
-{
-	struct mount *parent = mnt->mnt_parent;
-	struct mount *p = mnt, *m;
-#ifdef CONFIG_KDP_NS
-	struct super_block *sb = mnt->mnt->mnt_sb;
-#else
-	struct super_block *sb = mnt->mnt.mnt_sb;
-#endif
-
-	if (!sb->s_op->copy_mnt_data)
-		return;
-	for (p = propagation_next(parent, parent); p;
-				p = propagation_next(p, parent)) {
-#ifdef CONFIG_KDP_NS
-		m = __lookup_mnt(p->mnt, mnt->mnt_mountpoint);
-		if (m)
-			sb->s_op->copy_mnt_data(m->mnt->data, mnt->mnt->data);
-#else
-		m = __lookup_mnt(&p->mnt, mnt->mnt_mountpoint);
-		if (m)
-			sb->s_op->copy_mnt_data(m->mnt.data, mnt->mnt.data);
-#endif
-	}
-}
